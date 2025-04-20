308e30519f9ca58f8b03c8e8c807e00ae167855f
Farahghorab <Farahshherif@aucegypt.edu>
2025-04-20 20:20:29 +0200

    insertion functions

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -1,6 +1,6 @@
 #include "linkedlist.h"
 
-LinkedList::Node::Node(const std::string& val)
+LinkedList::Node::Node(const string& val)
     : data(val), next(nullptr)
 {}
 
@@ -35,7 +35,7 @@ bool LinkedList::isEmpty() const {
 bool LinkedList::isFull() const {
     Node* tmp = nullptr;
     try {
-        tmp = new Node(std::string());
+        tmp = new Node(string());
     } catch (...) {
         return true;
     }
@@ -47,23 +47,11 @@ int LinkedList::getLength() const {
     return length;
 }
 
-void LinkedList::insertAtEnd(const std::string& data) {
-    Node* newNode = new Node(data);
-    if (isEmpty()) {
-        head = newNode;
-    } else {
-        Node* cur = head;
-        while (cur->next && cur->next != head)
-            cur = cur->next;
-        cur->next = newNode;
-    }
-    ++length;
-}
 
-const std::string& LinkedList::getDataAt(int index) const {
-    static const std::string empty;
+const string& LinkedList::getDataAt(int index) const {
+    static const string empty;
     if (index < 0 || index >= length) {
-        std::cerr << "Error: index out of bounds\n";
+        cerr << "Error: index out of bounds\n";
         return empty;
     }
     Node* cur = head;
@@ -72,7 +60,7 @@ const std::string& LinkedList::getDataAt(int index) const {
     return cur->data;
 }
 
-int LinkedList::search(const std::string& value) const {
+int LinkedList::search(const string& value) const {
     Node* cur = head;
     int idx = 0;
     while (cur && idx < length) {
@@ -86,17 +74,17 @@ int LinkedList::search(const std::string& value) const {
 
 void LinkedList::printList() const {
     if (isEmpty()) {
-        std::cout << "List is empty\n";
+        cout << "List is empty\n";
         return;
     }
     Node* cur = head;
     int count = 0;
     while (cur && count < length) {
-        std::cout << cur->data << ' ';
+        cout << cur->data << ' ';
         cur = cur->next;
         ++count;
     }
-    std::cout << '\n';
+    cout << '\n';
 }
 
 void LinkedList::removeDuplicates() {
@@ -129,3 +117,104 @@ void LinkedList::makeCircular() {
         cur = cur->next;
     cur->next = head;
 }
+
+void LinkedList::insertAtFront(const string &data) {
+    Node* newNode = new Node(data); // new node creation
+    if (isEmpty()) //check if list is empty
+    {
+        head = newNode; //only one node which is the head
+    }
+    else
+    {
+        newNode->next = head; //new node points to head
+        head = newNode; //update head to become the new node
+    }
+    ++length;  //increase list size
+}
+
+
+void LinkedList::insertAtEnd(const string& data) {
+    Node* newNode = new Node(data); //creation of new node
+    if (isEmpty()) //check if list is empty
+    {
+        head = newNode; //only one node
+    }
+    else
+    {
+        Node* cur = head; //create pointer current to start from head
+        while (cur->next != nullptr && cur->next != head)//traverse through the list till last node
+        {
+            cur = cur->next; //to traverse through the list
+        }
+        cur->next = newNode; //insert new node at end
+    }
+    ++length; //increase list size
+}
+
+void LinkedList::insertAfter(Node *prevNode, const string &data) {
+   if (prevNode == nullptr) //checks if there is a previous node or not
+   {
+       cerr << "error: previous node is null" << endl;
+       return; //cannot insert after null
+   }
+    Node* newNode = new Node(data); //create a new node
+    newNode->next = prevNode->next; //new node points to next of previous node
+    prevNode->next = newNode; //to link the previous node to the new node
+
+    ++length; //increase list size
+}
+
+void LinkedList::insertAtPosition(int position, const string &data) {
+      if (position < 0 || position > length) //position not found
+      {
+          cerr << "error: position out of bounds" << endl;
+          return; //cannot insert at that position
+      }
+     if (position == 0) //if position is the first node(head)
+     {
+         insertAtFront(data);
+         return;
+     }
+    if (position == length) //if position is the last node(tail)
+    {
+        insertAtEnd(data);
+        return;
+    }
+    //if position is in between the list
+    Node* cur = head; //create a pointer to start from head
+    for (int i = 0; i < position - 1; i++) //traverse through the list until we reach the node before desired position
+    {
+        cur = cur->next;
+    }
+    //when desired position is reached
+    Node* newNode = new Node(data); //create new node
+    newNode->next = cur->next; //next of new node points to position
+    cur->next = newNode; //new node inserted at desired position
+
+    ++length;
+}
+
+void LinkedList::swapNodes(Node *node1, Node *node2) {
+     //if two nodes are the same , no need to swap
+    if (node1 == node2)
+        return;
+    Node*cur = head; //create a new pointer that starts at head
+
+    //swap the next pointers of each node
+    cur->next = node1->next;
+    node1->next = node2->next;
+    node2->next = cur->next;
+}
+
+//public getter to access nodes
+LinkedList::Node *LinkedList::getNodeAt(int index) const {
+    if (index < 0 || index >= length)
+        return nullptr;
+    Node* cur = head;
+    for (int i = 0; i < index; ++i)
+    {
+        cur = cur->next;
+    }
+    return cur;
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include "linkedlist.h"
+#include "LinkedList.h"
+using namespace std;
 
 int main() {
-    std::cout << "Beginning LinkedList Data Structure Tests:\n";
+    cout << "Beginning LinkedList Data Structure Tests:\n";
 
     LinkedList list;
 
-    std::cout << "\nTest 1: Check if list is empty initially:\n";
-    std::cout << "Is list empty? " << (list.isEmpty() ? "Yes" : "No") << "\n";
+    cout << "\nTest 1: Check if list is empty initially:\n";
+    cout << "Is list empty? " << (list.isEmpty() ? "Yes" : "No") << "\n";
 
-    std::cout << "\nTest 2: Inserting tokens at the end of the list:\n";
+    cout << "\nTest 2: Inserting tokens at the end of the list:\n";
     list.insertAtEnd("5");
     list.insertAtEnd("+");
     list.insertAtEnd("(");
@@ -17,41 +18,78 @@ int main() {
     list.insertAtEnd("*");
     list.insertAtEnd("3");
     list.insertAtEnd(")");
-    std::cout << "List after insertions: ";
+    cout << "List after insertions: ";
     list.printList();
 
-    std::cout << "\nTest 3: Getting the length of the list:\n";
-    std::cout << "Length of list: " << list.getLength() << "\n";
+    cout << "\nTest 3: Getting the length of the list:\n";
+    cout << "Length of list: " << list.getLength() << "\n";
 
-    std::cout << "\nTest 4: Retrieve data at index 3:\n";
-    const std::string& dataAtIndex = list.getDataAt(3);
-    std::cout << "Data at index 3: " << dataAtIndex << "\n";
+    cout << "\nTest 4: Retrieve data at index 3:\n";
+    const string& dataAtIndex = list.getDataAt(3);
+    cout << "Data at index 3: " << dataAtIndex << "\n";
 
-    std::cout << "\nTest 5: Searching for the first occurrence of token '+':\n";
+    cout << "\nTest 5: Searching for the first occurrence of token '+':\n";
     int foundIndex = list.search("+");
     if (foundIndex != -1)
-        std::cout << "Token '+' found at index: " << foundIndex << "\n";
+        cout << "Token '+' found at index: " << foundIndex << "\n";
     else
-        std::cout << "Token '+' not found in the list.\n";
+        cout << "Token '+' not found in the list.\n";
 
-    std::cout << "\nTest 6: Removing duplicate tokens:\n";
+    cout << "\nTest 6: Removing duplicate tokens:\n";
     list.removeDuplicates();
-    std::cout << "List after removing duplicates: ";
+    cout << "List after removing duplicates: ";
     list.printList();
-    std::cout << "Length after removing duplicates: " << list.getLength() << "\n";
+    cout << "Length after removing duplicates: " << list.getLength() << "\n";
 
-    std::cout << "\nTest 7: Making the list circular:\n";
+    cout << "\nTest 7: Making the list circular:\n";
     list.makeCircular();
-    std::cout << "List after converting to circular: ";
+    cout << "List after converting to circular: ";
     list.printList();
 
-    std::cout << "\nTest 8: Check if list is full:\n";
-    std::cout << "Is list full? " << (list.isFull() ? "Yes" : "No") << "\n";
+    cout << "\nTest 8: Check if list is full:\n";
+    cout << "Is list full? " << (list.isFull() ? "Yes" : "No") << "\n";
 
-    std::cout << "\nTest 9: Retrieve data at an out of bounds index (index 100):\n";
-    const std::string& invalidData = list.getDataAt(100);
-    std::cout << "Correctly handled out of bounds request\n";
+    cout << "\nTest 9: Retrieve data at an out of bounds index (index 100):\n";
+    const string& invalidData = list.getDataAt(100);
+    cout << "Correctly handled out of bounds request\n";
 
-    std::cout << "\nAll Tests Completed\n";
+    cout << "\nTest 10: Inserting tokens at the start of the list:\n";
+    list.insertAtFront("4");
+    list.insertAtFront("+");
+    list.insertAtFront("(");
+    list.insertAtFront("2");
+    list.insertAtFront("*");
+    list.insertAtFront("3");
+    list.insertAtFront("+");
+    list.insertAtFront("5");
+    list.insertAtFront("-");
+    list.insertAtFront("7");
+    list.insertAtFront(")");
+    cout << "List after insertions: ";
+    list.printList();
+
+    cout << "Length of list after insertions: " << list.getLength() << endl;
+
+    cout << "\nTest 11: Inserting node with value (3) after node containing value (2):" << endl;
+
+    LinkedList::NodePtr node1 = list.getNodeAt(2); //to get node at index 2
+    LinkedList::NodePtr node2 = list.getNodeAt(3); //to get node at index 3
+
+    list.insertAfter(node1, "3"); //insert value 3 (node with name node2) after node containing the value 2 (node1)
+    cout << "List after insertions: ";
+    list.printList();
+
+    cout << "\nTest 12: Inserting node at position 2:" << endl;
+    list.insertAtPosition(2,"A"); //insert the data (A) at position 2
+    cout << "List after insertions: ";
+    list.printList();
+
+    cout << "\nTest 13: swapping node1 with node2:" << endl;
+    list.swapNodes(node1, node2);
+    cout << "List after swapping nodes: ";
+    list.printList();
+
+
+    cout << "\nAll Tests Completed\n";
     return 0;
 }
