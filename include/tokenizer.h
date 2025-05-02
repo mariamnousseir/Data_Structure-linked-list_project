436c5b03d7a28e2749132c36d067c3b7a1dba4dd
Farahghorab <Farahshherif@aucegypt.edu>
2025-05-03 01:21:28 +0300

    tokenizer(popFront, reverse, tokenize)

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -48,3 +48,60 @@ public:
     bool isEmpty() const {          // Check if the list is empty
         return head == nullptr;     // True if head is null
     }
+
+        string popFront() {             // Remove and return the token at the front of the list
+        if (isEmpty())              // If list is empty
+            return string();        // Return empty string
+        TokenNode* node = head;     // Create new node to remove
+        string tok = node->token;   // Copy its token
+        head = node->next;          // Advance head to next node
+        if (!head)                  // If list is now empty
+            tail = nullptr;         // Reset tail as well
+        delete node;                // Delete the removed node
+        return tok;                 // Return the token
+    }
+
+    void reverse() {                // Reverse the order of nodes in the list
+        TokenNode* prev = nullptr;  // Previous node pointer (none at start)
+        TokenNode* cur = head;      // Current node pointer starts at head
+        tail = head;                // After reversal, original head becomes the tail
+        while (cur) {               // Traverse through the list
+            TokenNode* nxt = cur->next; // Store next node
+            cur->next = prev;       // Reverse the link
+            prev = cur;             // Move prev up
+            cur = nxt;              // Move to next node
+        }
+        head = prev;                // After loop, prev is new head
+    }
+};
+
+// tokenize: convert an expression string into a linked list of tokens
+inline TokenList* tokenize(const string& expr) {
+    TokenList* list = new TokenList(); // Create a new, empty TokenList
+    size_t i = 0;                       // Index to traverse the expression string
+    while (i < expr.size()) {          // Loop until end of string
+        if (isspace(expr[i])) {        // Skip whitespace characters
+            ++i;
+            continue;
+        }
+        if (isdigit(expr[i]) || expr[i] == '.') {  // Number (integer or floating point)
+            string num;
+            while (i < expr.size() && (isdigit(expr[i]) || expr[i] == '.'))
+                num.push_back(expr[i++]); // Accumulate number characters
+            list->append(num);         // Append the complete number token
+        }
+        else if (isalpha(expr[i])) {   // Identifier (variable/function name)
+            string var;
+            while (i < expr.size() && isalnum(expr[i]))
+                var.push_back(expr[i++]); // Accumulate alphanumeric characters
+            list->append(var);         // Append the complete identifier token
+        }
+        else {                         // Operator or parenthesis (any other single char)
+            list->append(string(1, expr[i++])); // Append the single-character token
+        }
+    }
+    return list;                       // Return the populated TokenList
+}
+
+#endif // TOKENIZER_H               // Include guard end
+
